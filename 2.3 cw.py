815d9c4bbac211eea2d84e9abef2241921119c84
Mohit819 <immohitrana06@gmail.com>
2024-09-17 18:02:55 -0400

    Add files via upload

---
+++ 2.3 cw.py
@@ -0,0 +1,37 @@
+##movie_name = "Turning Red" # strings need quotes
+##year_released = 2022 # int type (whole number)
+##rating = 4.6 # float type (decimal number)
+##best_band = "4-Town" # no spaces allowed in variable names
+##print(movie_name) # what will this line print out?
+##name = input ('What is your name?')
+##print (name)
+##decimal = input ('Enter a decimal number:')
+##print (decimal)
+
+##
+##x = float(input("Enter a decimal number: ")) # x stores a float
+##print(x * 2) # so we can use it in math
+##a = int(input("Enter a whole number: ")) # a is of type int
+##b = int(input("Enter a whole number: ")) # b is also an int
+##print(a + b) # we can do math with 2 ints!
+##
+##
+##c = 5.8
+##print(int(c))
+
+
+##CALCULATOR##
+
+y = int(input('Enter a number'))
+z = int(input('Enter a number'))
+operator = input("Which operation(+,-,*,/)")
+
+if operator == "+":
+    print (y+z)
+elif operator ==- "-":
+    print(y-z)
+elif operator ==-"*":
+    print(y*z)
+else:
+    print(y/z)
+    
