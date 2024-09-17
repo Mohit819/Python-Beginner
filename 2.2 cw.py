71f7c0b0fbad9481fb0f9872af0dcce4f523c06d
Mohit819 <immohitrana06@gmail.com>
2024-09-17 17:24:07 -0400

    Add files via upload

---
+++ 2.2 cw.py
@@ -0,0 +1,41 @@
+print("Hello World!")
+print("Monday\n\tTuesday\n\t\tWednesday\n\t\t\tThurday\n\t\t\t\tFriday")
+print("Saturday\tSunday")
+
+
+
+first_int = 2
+# the name, an equals sign, and then your number!
+second_int = 40
+third_int = -99
+# negative numbers are integers too if they are whole
+first_float = 2.5
+# the name, an equals sign, and then your number!
+second_float = 0.00000001
+third_float = -99.9 # negative numbers can be floats too
+name = "buzz lightyear" # strings have quotations around them
+famous_quote = "To infinity, and beyond!" # they can have spaces
+birthday = "October 28, 2000" # they can have numbers too
+best_movie = 'Toy Story II' # single quotes work too
+age = 10
+print(age)
+# no quotation marks since we are using our variable here!
+# let's increase our age!
+age = age + 1 # this is like saying age = 10 + 1, or age = 11
+print(age) # this one will print 11
+print('A variable is where you can store something')
+bob = 1
+bub = 1.1
+bib = 'one'
+#bob@bing.com = 11
+beb = "34"
+birthday = 'June 6'
+#1. STRING
+#2. FLOAT
+#3. INTEGER
+#4. INTEGER
+#5. STRING
+#6. FLOAT
+#7. INTEGER
+#8. INTEGER
+#9. FLOAT
