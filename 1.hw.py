c2356d3e68734f24179f98131ef8ed2ca5cb4528
Mohit819 <immohitrana06@gmail.com>
2024-09-16 20:31:49 -0400

    Add files via upload

---
+++ 1.hw.py
@@ -0,0 +1,8 @@
+# any line that starts with a hashtag is called a comment
+# comments are like notes you can leave for yourself
+# Python will not attempt to read it as code
+
+#I want to learn to code because it can help me when I need it in the future.
+print('I want to make a functioning website.')
+print('Codings useful for making websites\nIts useful for making softwares\nCoding is also useful for understanding code better\n Its useful for teaching code\nIts useful for making an AI')
+print('FALLING UP\n\nAnd I fell up\n\nUp to the roof tops,\n\n\tUp over the town,\n\n\t\tUp past the tree tops,\n\n\t\t\tUp over the mountains,\n\n\t\t\t\tUp where the colours,\n\n\t\t\t\t\tBlend into the sounds.\n\n\t\t\t\tBut it got me so dizzy\n\n\t\t\tWhen I looked around,\n\n\t\tI got sick to my stomach\n\n\tAnd I threw down')
