afa1e0f11bb9201201895276c7a5ea1f0b247e40
Mohit819 <immohitrana06@gmail.com>
2024-09-23 19:45:26 -0400

    Add files via upload

---
+++ 2.2 hw.py
@@ -0,0 +1,9 @@
+#$_in_account v@lue print Last name
+#money in account value Print Last_name
+#1. STRING 2.FLOAT 3.FLOAT 4.STRING 5.STRING 6.INT 7.STRING 8.STRING 9.FLOAT
+Name_of_movie = "Avengers Infinity War"
+Year = "2018"
+Month = "April"
+My_rating = "8/10"
+Length = "2 hours 29 minutes"
+Additional_info = "It's the 3rd movie in series"
