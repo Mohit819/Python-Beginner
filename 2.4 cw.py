2ca02bc4b9547b22664dd18a3e4dbe3452690176
Mohit819 <immohitrana06@gmail.com>
2024-10-01 17:59:56 -0400

    Add files via upload

---
+++ 2.4 cw.py
@@ -0,0 +1,24 @@
+##year = int(input("What year is it now? "))
+##birth_year = int(input("What year were you born? "))
+##age = year - birth_year
+##print("You are probably", age, "years old!")
+##change_in_wallet = 15.65
+##birthday_money = 20.00
+##total_money = change_in_wallet + birthday_money
+##a = 9 * 2 + 4/2
+##b = 20 + 10/5
+##A function is when you give the code an input and the code gives you an output.
+##You might get a TypeError if you do a typo in your code
+##a = str(input("Write any word you want"))
+##b = str(input("Write any word you want"))
+##c = str(input("Write any word you want"))
+##length = len(a)
+##length2 = len(b)
+##length3 = len(c)
+##print(length+length2+length3)
+##x = sum((1,2,3,4,5,6,7,8,9,10))
+##print(x)
+x = int(input("What are your marks in Math?"))
+y = int(input("What are your marks in Science?"))
+z = int(input("What are your marks in Language?"))
+print(max(x,y,z))
