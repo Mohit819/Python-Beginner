76cd73897e52e220936c15b569dc6b2beddb1dca
Mohit819 <immohitrana06@gmail.com>
2024-10-01 16:31:15 -0400

    Update 2.3 hw.py

--- 2.3 hw.py
+++ 2.3 hw.py
@@ -2,3 +2,14 @@
 ##print( 15 + 10 / 2 - 1)
 ##print( (3 * 3) - (2 * 2 / 2))
 ##print( 9 / (5 - 2) + 6)
+##x = int(input("Enter a number"))
+##y = int(input("Enter a number"))
+##z = int(input("Enter a number"))
+##print(x*y*z)
+##a = 3005
+##b = 4.75
+##a = float(input("Enter a decimal number"))
+##b = float(input("Enter a decimal number"))
+##c = float(input("Enter a decimal number"))
+print(100*"hi")
+
