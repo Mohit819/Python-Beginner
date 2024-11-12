e00911543726142db7db75492a1a1686d546fa47
Mohit819 <immohitrana06@gmail.com>
2024-11-12 17:49:45 -0500

    Add files via upload

---
+++ 2.5 cw.py
@@ -0,0 +1,51 @@
+##num1 = min(2.5, 0, -10, int("15")) # pull up the last worksheet
+##num2 = max( 1, round(4.1), 12) # if you don't remember these
+##print(sum((num1, num2)))
+
+##a = "hello"
+##b = "h"
+##
+##print("" in b)
+
+##print( True and False)
+### This will print False, since both sides are not True
+##print( True or False) # This is True since one side is True
+##print( False and False) # What will these print?
+##print( False or False)
+##print( True or True)
+##boolean = True and True # what will be stored in this variable?
+
+##print( 12 == 12 and "cat" in "catastrophe")
+### 12 == 12 is True, "cat" in "catastrophe" is True
+### True and True gives us True
+##print( 3 > 5 or 9 > 5)
+### 3 > 5 is False, 9 > 5 is True, False or True gives us True
+##name = "Samuel"
+##age = 14
+##print( "u" in name and age < 10)
+###"u" in name is asking if there is a "u" in "Samuel", True
+### age < 10 is False since age is 14, True and False = False
+##print("j" in name or age == 14)
+
+##print (3 == 5)
+##print (4<10)
+##print (3!=3)
+##print ("C" in "abc")
+##print ("bat" in "batch")
+##print ("river" == "river")
+
+##print("A" in "HAM" and 3 < 0)
+##print( 2 * 2 == 4 or 2 * 2 == 8)
+##print( "j" not in "JEREMY" and 3 < 4)
+##print(3 + 4 == 2 + 5 or 3 == 3)
+##print(not "map" in "amp")
+##print( 22 == 11*2 or 22 == 2 + 2)
+
+##a = input("Write one letter.")
+##print (a in "abcdefghijklmnopqrstuvwxyz")
+
+a=(5)
+b=(10)
+c=(1)
+
+print(a == b + 3 or a + 5 != 12) and (a + b < 100 and b > c)
