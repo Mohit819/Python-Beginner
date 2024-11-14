af348011c594a4c49628526d0b68768c7c636de2
Mohit819 <immohitrana06@gmail.com>
2024-11-14 16:41:38 -0500

    Add files via upload

---
+++ 2.4 hw.py
@@ -0,0 +1,12 @@
+#You might get a name error because you are writing the functions with an uppercase letter for example Min.
+##num = "4.2"
+##num2 = round(float(num))
+##print(num2 * int(3.4))
+##a = input("Enter a decimal number")
+##b = input("Enter a decimal number")
+##c = input("Enter a decimal number")
+##print(min(a,b,c))
+##print(len("You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose."))
+##print(sum(3.45,-99.45,1004.2,32,-1.234,67.555))
+a = (input("Write the longest number you can think of"))
+print(len(a))
