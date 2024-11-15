331e79e649b197298c6677637bed839c7e98ffb4
Mohit819 <immohitrana06@gmail.com>
2024-11-14 19:58:56 -0500

    Add files via upload

---
+++ 2.7 cw.py
@@ -0,0 +1,91 @@
+##name = input("What is your name?")
+##if len(name) > 8:
+##    print("Your name is quite long!")
+##elif len(name) < 4:
+##    print("Your name is quite short!")
+##elif name == "Jakob":
+##    print("We have the same name!")
+##else:
+##    print(name, "is a lovely name!")
+
+# each piece of code here will cause a NameError
+##my_favourite_day = "Friday"
+##print("My favourite day of the week is", favourite_day)
+### look closely! There is no variable called favourite_day here
+##print(Hello) # it's will try to find a variable called Hello
+##pint("Hello World!"
+
+# each line will make a TypeError
+##bool_1 = 3.1415 < 'pi'
+##remainder = "45" - 40
+##a = round(“string”) # the round function is expecting a number
+
+###Correct
+##print("Hello World")
+###Incorrect
+##print "Hello World!"
+##print( "Hello World!"
+##(print "Hello World!")
+
+###CORRECT:
+##address = “123 street rd.”
+###INCORRECT:
+##address = 123 street rd.’
+##address = “123 street rd.
+##address = ‘123 street rd.”
+
+###CORRECT:
+##answer = ((3 + 4) * (-2) *2)
+###INCORRECT:
+##answer = ((3+4) * (-2)*2
+##answer = (3 + 4 * (-2) * 2
+##answer = (3 * 4) * (-2) * 2)
+
+##int1 = int(input("Enter x: "))
+##int2 = int(input("Enter y: "))
+##print("x * y = ", x + y)
+##print("x + y = ", x * y)
+
+#A name error happens when python is trying to find something that doesn't exist.
+#A type error happens when you use an operation on a string type.
+#An Indentation error happens when you forget to do an indent or do a random indent
+#A syntax error happens when you forget to put brackets or quotation marks in your code.
+#A logic error happens when the code is fine but the code doesn't do what we want it to.
+
+##my_age(10)
+##print("age")
+
+##print("4"+4)
+##print("one"-1)
+
+##print hi
+##print(hi)
+##print"hi"
+
+##num = int(input("Enter a number"))
+##if num < 0:
+##print("thats a negative number")
+##else
+##print("thats not a negative number")
+
+##x = (1)
+##y = (1)
+##print("x + y")
+
+##print('The never-ending string!)
+###the quotation mark isnt closed
+##val = "3" > print("hello!")
+###there arent brackets on the first part and its using an operation on a string
+##PRINT("Nonexistent function!")
+###the print function is uppercase
+##name = Holly
+###there are no brackets or quotation marks
+
+##age = int(input("How old are you?"))
+##height = float(input("How tall are you? (in CM)"))
+##if age >= 14 and height > 140:
+##    print("You are allowed on the rollercoaster!")
+##else:
+##    print("You are not allowed on this ride")
+
+
