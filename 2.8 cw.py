799f793c6cc99542d3dc7b17011aec1755a83d7a
Mohit819 <immohitrana06@gmail.com>
2024-11-21 18:40:26 -0500

    Add files via upload

---
+++ 2.8 cw.py
@@ -0,0 +1,80 @@
+##height = int(input("How tall are you? (in inches)"))
+##if height < 48:
+##    print("You are quite small")
+##elif height == 64:
+##    print("You are the height of the average Canadian woman")
+##elif height == 69:
+##    print("You are the height of the average Canadian man")
+##elif height > 69:
+##    print("You are on the tall side")
+
+##for letter in "Hello World":
+##    print(letter)
+
+##for i in range(2,9):
+##    print(i)
+##for i in range(2,9):
+##    print(i + 1)
+##for num in range(5):
+##    print("!")
+##num = 0
+##for i in range(10):
+##    num = num + 10 
+##print(num)
+
+##for i in range(5):
+##    print(i * "#")
+##for character in "Horton Hears a Hoo":
+##    if character != "H": 
+##        print(character)
+##for letter in "The Quick Brown Fox Jumps Over The Lazy Dog":
+##    if letter in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+##        print(letter)
+##for i in range(0, 100, 3):
+##    print(i)
+##a_count = 0
+##for letter in "Samantha":
+##    if letter == 'a':-
+##        a_count = a_count + 1
+
+##for a in range(10):
+##    print("Mohit")
+
+##for hi in "im still standing":
+##    if hi in "aeiou":
+##        print(hi)
+##sum1=0
+##for i in range(100):
+##    sum1 = sum1 + i
+##print(sum1)
+
+##b_count = 0
+##for i in "Barry_B._Benson":
+##    if i == "B":
+##        b_count = b_count + 1
+##print(b_count)
+
+##letter = 0
+##bob = input("Write your name")
+##for i in bob:
+##    letter=letter+1
+##print(letter)
+
+##i = 0
+##for i in range(0,20,1):
+##    i = i + 1
+##    print(i)
+
+##for i in range(30,0,-1):
+##    i = i -1
+##    print(i)
+
+##bob = 0
+##for i in "pneumonoultramicroscopicsilicovolcanoconiosis":
+##    bob = bob + 1
+##print(bob)
+
+for bob in range (1,5):
+    print("#"*bob)
+for bob in range(3,0,-1):
+    print("#"*bob)
