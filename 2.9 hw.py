3f8b1d3227a67845a992a5fbe7a9345bdd718a8d
Mohit819 <immohitrana06@gmail.com>
2024-11-26 17:42:46 -0500

    Update 2.9 hw.py

--- 2.9 hw.py
+++ 2.9 hw.py
@@ -3,8 +3,39 @@
 ##    i = input("enter a 10 digit number")
 ##print (i)
 
-i = input("enter a password")
-for w in i:
-    while w != "+- % $ # @":
-        i = input("enter a password")
-        
+##symbol = "+-%$*&!^()=_/#@<>?[]{}%"
+##password = input("enter a password")
+##check=False
+##attempts=1
+##for letter in password:
+##    if letter in symbol:
+##        check=True
+##
+##while check != True:
+##    password = input("enter a password")
+##    attempts +=1
+##    if attempts == 5:
+##        break
+##    for letter in password:
+##        if letter in symbol:
+##            check=True
+
+##for i in range(3,100,3):
+##    print(i)
+
+##i = 0
+##while i <99:
+##    i += 3
+##    print(i)
+
+##num = 7
+##guess = int(input("enter a number from 1-9"))
+##while guess !=num:
+##    guess = int(input("enter a number from 1 - 9"))
+##    if guess >8:
+##        print("please enter a valid number")
+
+hi = True
+while hi == False:
+    print("hi")
+
