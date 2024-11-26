55102c0bcbed45c9a8e650ebbcb654320cca0813
Mohit819 <immohitrana06@gmail.com>
2024-11-26 16:17:12 -0500

    Add files via upload

---
+++ 2.8 hw.py
@@ -0,0 +1,25 @@
+##for i in range(1,7):
+##    print("#"*i)
+##for i in range(7,0,-1):
+##    print("#"*i)
+
+##y = 0
+##i = input("what is your favourite letter")
+##w = input("what is you favourite word")
+##for a in w:
+##    if a==i:
+##        y = y+1
+##print(y)
+
+##z=0
+##i = input("ENter a sentence")
+##for w in i:
+##    if w !="qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM":
+##        z = z+1
+##print(z)
+
+##for i in range(5,100,5):
+##    print(i)
+
+#iterator variables are variables that change value every time
+# the range fuction takes 1 2 or 3 inputs and gives an output
---
+++ 2.9 hw.py
@@ -0,0 +1,10 @@
+##i = input("enter a 10 digit number")
+##while len (i) != 10:
+##    i = input("enter a 10 digit number")
+##print (i)
+
+i = input("enter a password")
+for w in i:
+    while w != "+- % $ # @":
+        i = input("enter a password")
+        
